470ae480aa5a5a33119a3fc5cfbc32795cb07788
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-03-18 22:25:42 +0530

    Insert

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open Youtube
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
