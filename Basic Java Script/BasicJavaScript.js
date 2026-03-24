3633f7c899a62f7af0e426481a3acb6baf97d49c
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-03-24 13:04:53 +0530

    Insert

---
+++ Basic Java Script/BasicJavaScript.js
@@ -0,0 +1,7 @@
+console.log('VS'+' Code');
+console.log('Hello'+' '+'World');
+console.log('Output: '+'Done');
+
+console.log('First message');
+console.log('Second message');
+console.log('Third message');
