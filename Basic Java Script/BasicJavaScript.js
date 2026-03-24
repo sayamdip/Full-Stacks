d44010d82521d33f5821bf89c88425b1a1ecfb26
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-03-24 21:52:28 +0530

    Insert

--- Basic Java Script/BasicJavaScript.js
+++ Basic Java Script/BasicJavaScript.js
@@ -5,3 +5,12 @@ console.log('Output: '+'Done');
 console.log('First message');
 console.log('Second message');
 console.log('Third message');
+
+
+// This Is A Comment
+
+/* This 
+Is A Multi-Line 
+Comment */
+
+/* This Is A Multi-Line Comment In A Single Line */
