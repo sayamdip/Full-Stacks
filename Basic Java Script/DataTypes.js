fcd22c1797874b462d62067b5d21f36dd81ddbfc
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-03-25 15:20:08 +0530

    Insert

---
+++ Basic Java Script/DataTypes.js
@@ -0,0 +1,10 @@
+let ticketPrice=120;
+let ticketPriceText="120";
+let totalCost=ticketPrice+30;
+let combinedText=ticketPriceText+"30";
+
+console.log(totalCost+" "+combinedText+" "+typeof ticketPrice+" "+typeof ticketPriceText);
+
+let bool2=true;
+let bool3=true;
+console.log(bool2*bool3);
