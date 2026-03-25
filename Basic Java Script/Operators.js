f73c69ded7f8d0275e6686d58564e2637c356226
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-03-25 20:44:44 +0530

    Insert

--- Basic Java Script/Operators.js
+++ Basic Java Script/Operators.js
@@ -14,6 +14,12 @@ console.log(a*b);
 console.log("4) Division");
 console.log(a/b);
 
+console.log("5) Modulo")
+console.log(b%a); // Gives The Number In The Reminder After Division
+
+console.log("6) Power");
+console.log(a**3);
+
 console.log("Comparison Operator");
 console.log(a>b);
 console.log(a<b);
---
+++ Basic Java Script/String.js
@@ -0,0 +1,6 @@
+let a="It Is A Lovely Day. "
+let b="My Name Is Sayamdip Dey Chaklader."
+
+console.log("String Concatenation\n");
+let c=a+b;
+console.log(c); // Always Use Double Quotation As With It You Can Write Sentences
