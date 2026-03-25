f9fd70be607539a78b67c19b8e42f1a9a948aaa6
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-03-25 20:18:58 +0530

    Insert

---
+++ Basic Java Script/Operators.js
@@ -0,0 +1,20 @@
+let a=5;
+let b=9
+
+console.log("Arithmetic Operators");
+console.log("1) Addition");
+console.log(a+b);
+
+console.log("2) Subtraction");
+console.log(a-b);
+
+console.log("3) Multiplication");
+console.log(a*b);
+
+console.log("4) Division");
+console.log(a/b);
+
+console.log("Comparison Operator");
+console.log(a>b);
+console.log(a<b);
+console.log(a==b);
