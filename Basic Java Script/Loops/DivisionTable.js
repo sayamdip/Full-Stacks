cad6e969f74839e0582bbb3de564f225d13456e6
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-04-20 23:53:37 +0530

    Insert

---
+++ Basic Java Script/Loops/DivisionTable.js
@@ -0,0 +1,5 @@
+for(var i=100;i>10;i=i-10){
+    for(var j=10;j>4;j=j-5){
+        console.log(i+" Divided By "+j+" Equals "+(i/j));
+    }
+}
---
+++ Basic Java Script/Loops/Exercise1.js
@@ -0,0 +1,31 @@
+for(let i=1;i<=10;i++){
+    if(i==1){
+        console.log("Gold medal");
+    }
+    else if(i==2){
+        console.log("Silver medal");
+    }
+    else if(i==3){
+        console.log("Bronze medal");
+    }
+    else{
+        console.log(i);
+    }
+}
+
+for (let j=1;j<=10;j++){
+    switch(j){
+        case 1:
+            console.log("Gold medal");
+            break;
+        case 2:
+            console.log("Silver medal");
+            break;
+        case 3:
+            console.log("Bronze medal");
+            break;
+        default:
+            console.log(j);            
+
+    }
+}
