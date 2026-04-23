6bc27fda8ea85810eea9db482be7090d493b4a0e
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-04-23 23:40:31 +0530

    Insert

--- Basic Java Script/Objects/MathObject.js
+++ Basic Java Script/Objects/MathObject.js
@@ -5,3 +5,17 @@ var Euler=Math.E;
 var Logarithm2=Math.LN2;
 
 // Rounding Methods
+var a=Math.ceil(5.4); // Rounds Up To The Closest Integer
+var b=Math.floor(5.4); // Rounds Down To The Closest Integer
+var o=Math.round(5.4); // Rounds Up To The Closest Integer If The Decimal Is .5 Or Above; Otherwise, Rounds Down To The Closest Integer
+var p=Math.truncate(5.4) // Trims The Decimal, Leaving Only The Integer
+
+// Arithmetic And Calculus Methods
+var pow=Math.pow(2,3);
+var sqrt=Math.sqrt(16);
+var cbrt=Math.cbrt(8);
+var abs=Math.abs(-10);
+
+var min=Math.min(9,8,7);
+var max=Math.max(9,8,7);
+var log=
