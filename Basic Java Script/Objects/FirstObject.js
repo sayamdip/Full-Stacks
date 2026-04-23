3b1985f70c3ea514973b526905bd76ae41cf0806
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-04-23 20:53:58 +0530

    Insert

---
+++ Basic Java Script/Objects/FirstObject.js
@@ -0,0 +1,16 @@
+let storeManager={}; // This Is The Object;
+// These Are The Properties
+storeManager.rangeTilesPerTurn=4;
+storeManager.socialSkills=50;
+storeManager.streetSmarts=50;
+storeManager.health=30;
+storeManager.specialAbility="Finding Business Opportunities";
+storeManager.greeting="Let's Make Some Money";
+
+var assistantManager={ // This Is The Object
+    // These Are The Properties
+    movement:3,
+    socialSkills:30,
+    streetSmarts:30,
+    health:40
+}
