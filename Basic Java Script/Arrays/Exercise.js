5e0c7bb79a7ae542b103168635c2b1ce1ccd37b3
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-04-24 12:49:00 +0530

    Insert

---
+++ Basic Java Script/Arrays/Exercise.js
@@ -0,0 +1,15 @@
+let clothes=[];
+clothes.push("Jeans");
+clothes.push("T-Shirt");
+clothes.push("Suit");
+clothes.push("Boxers");
+clothes.push("Shirt");
+console.log(clothes);
+clothes.pop();
+console.log(clothes);
+console.log(clothes[2]);
+
+var favCar={};
+favCar.color="Blue";
+favCar.convertible=true;
+console.log(favCar);
---
+++ Basic Java Script/Strings/StringMethods.js
@@ -0,0 +1,12 @@
+var greet="Hello, ";
+var place="World";
+
+// String Methods
+console.log(greet.charAt(0));
+console.log("Wo".concat("rl".concat("d")));
+console.log("ho-ho-ho".indexOf("h"));
+console.log("ho-ho-ho".indexOf("o"));
+console.log("ho-ho-ho".indexOf("-"));
+console.log("ho-ho-ho".split("-"));
+console.log(greet.toUpperCase());
+console.log(greet.toLowerCase());
