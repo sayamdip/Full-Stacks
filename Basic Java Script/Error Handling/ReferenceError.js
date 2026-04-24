d302a903d132e3419a31fbc40e9e1dbd3d43f3ed
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-04-24 19:53:36 +0530

    Insert

---
+++ Basic Java Script/Error Handling/ReferenceError.js
@@ -0,0 +1,8 @@
+// Error
+console.log(c+d); // c And d Is Not Defined
+console.log("This Line Never Runs");
+
+// Bug
+var a="4";
+var b=5;
+console.log(a+b); // This Gives 45 As A String
---
+++ Basic Java Script/Error Handling/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+// Error
+// console.log(c+d); // c And d Is Not Defined
+// console.log("This Line Never Runs");
