91adb03a6080666b60e38b0ea12df754158111e9
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-04-24 13:21:35 +0530

    Insert

---
+++ Basic Java Script/Basic Java Script/TypeOfOperator.js
@@ -0,0 +1,11 @@
+var test1=typeof(4);
+var test2=typeof(3.14);
+var test3=typeof(true);
+var test4=typeof("Sayamdip");
+var test5=typeof([1,2,3,4]);
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
+console.log(test5);
