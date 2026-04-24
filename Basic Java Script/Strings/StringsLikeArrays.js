c0eaf43c8f40d9f339f141148cc434fc913b3da2
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-04-24 12:28:56 +0530

    Insert

--- Basic Java Script/Strings/StringsLikeArrays.js
+++ Basic Java Script/Strings/StringsLikeArrays.js
@@ -2,3 +2,9 @@ var letters="abc";
 for(var i=0;i<letters.length;i++){
     console.log(letters[i]);
 }
+
+// Concat Method
+var great="Hello ";
+var user="Lisa";
+console.log(great+user);
+console.log(great.concat(user));
