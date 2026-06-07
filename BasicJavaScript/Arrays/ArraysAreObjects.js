9609ea4ab2a1ca2ea1d1e25eec366e388bb100d0
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-06-07 15:58:55 +0530

    Insert

--- BasicJavaScript/Arrays/ArraysAreObjects.js
+++ BasicJavaScript/Arrays/ArraysAreObjects.js
@@ -1,22 +1,66 @@
-// push() Operations 
+const myArr = [0 ,1 ,2 ,3 ,4]
 
-// Example 1
+console.log(myArr[3])
 
-var fruits=[];
-fruits.push("Apple"); // To Add Item In An Array
-fruits.push("Jackfruit");
-console.log(fruits);
+const myHeroes = ["Shaktiman", "Naagraj", "Ironman"]
 
-// Example 2
+const myArr3 = new Array(1,2,3,4)
+console.log(myArr[3])
 
-function arrayBuilder(one,two,three){
-    var arr=[];
-    arr.push(one);
-    arr.push(two);
-    arr.push(three);
+myArr.push(5)
+console.log(myArr)
 
-    arr.pop(); // pop() Operation
-    arr.pop();
-    return arr;
-}
-console.log(arrayBuilder("Apple","Pear","Plum"));
+
+myArr.pop()
+console.log(myArr)
+
+myArr.unshift(9)
+console.log(myArr)
+
+
+myArr.shift()
+console.log(myArr)
+
+console.log(myArr.includes(9))
+console.log(myArr.indexOf(9))
+console.log(myArr.indexOf(3))
+
+const newArr = myArr.join()
+
+console.log(myArr)
+console.log(typeof newArr)
+
+// Slice And Splice
+console.log("A ", myArr)
+
+const myn = myArr.slice(1,3)
+console.log("B ",myn)
+
+const mynew = myArr.splice(1,3)
+console.log("C ", myArr)
+console.log(mynew)
+
+// Concat And Spread
+
+const marvel_heroes = ["Thor", "Ironman", "Spiderman"]
+const dc = ["Superman", "Flash", "Batman"]
+
+const allHeroes = marvel_heroes.concat(dc)
+console.log(allHeroes)
+
+const all_new_heroes = [...marvel_heroes, ...dc]
+console.log(all_new_heroes)
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array)
+
+console.log(Array.isArray("Sayamdip"))
+console.log(Array.from("Sayamdip"))
+console.log(Array.from({name: "Sayamdip"}))
+
+let score = 400
+let value = 500
+console.log(Array.of(score,value))
+
+
