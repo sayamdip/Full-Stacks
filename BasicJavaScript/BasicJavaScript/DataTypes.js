631822eac0797bedf396f2a1ef9c92cbfc127f7a
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-06-07 13:13:06 +0530

    Insert

--- BasicJavaScript/BasicJavaScript/DataTypes.js
+++ BasicJavaScript/BasicJavaScript/DataTypes.js
@@ -1,10 +1,43 @@
-let ticketPrice=120;
-let ticketPriceText="120";
-let totalCost=ticketPrice+30;
-let combinedText=ticketPriceText+"30";
+// -------- Primitive Data Types --------
 
-console.log(totalCost+" "+combinedText+" "+typeof ticketPrice+" "+typeof ticketPriceText);
+// 7 Types: String, Number, Boolean, BigInt, Symbol, Undefined, Null
 
-let bool2=true;
-let bool3=true;
-console.log(bool2*bool3);
+// -------- Reference (Non-Primitive) Data Types --------
+
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+const id = Symbol("1234")
+const anotherId = Symbol("1234")
+
+console.log(id === anotherId)
+
+
+const bigNumber = 34567894567878n //BigInt
+
+
+// Array, Objects, Functions
+
+const heroes = ["Shaktiman", "Naagraj", "Doga"]
+
+
+let myObj = {
+    name: "Sayamdip",
+    age: 23
+}
+
+const myFunction = function () {
+    console.log("Hello World!")
+}
+
+
+console.log(typeof myObj)
+
+
+console.log(typeof myFunction)
--- BasicJavaScript/BasicJavaScript/String.js
+++ BasicJavaScript/BasicJavaScript/String.js
@@ -1,6 +1,24 @@
-let a="It Is A Lovely Day. "
-let b="My Name Is Sayamdip Dey Chaklader."
+let a = "It Is A Lovely Day. "
+let b = "My Name Is Sayamdip Dey Chaklader."
 
 console.log("String Concatenation\n");
-let c=a+b;
+let c = a + b;
 console.log(c); // Always Use Double Quotation As With It You Can Write Sentences
+
+const name = "Sayamdip"
+const repoCount = 50
+
+
+console.log(`Hello My Name Is ${name} And My RepoCount Is ${repoCount}`)
+
+const gameName = new String("Sayamdip")
+
+console.log(gameName)
+
+// ---- String Methods -----
+
+console.log(gameName[0])
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(4))
+console.log(gameName.indexOf("t"))
