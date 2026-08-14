7f8aa14d2a7e39b228dd065893a8fc1e60b2f371
Sayamdip@ghost141 <143872527+sayamdip@users.noreply.github.com>
2026-08-14 10:04:29 +0530

    Insert

---
+++ JavaScript/Promises And Fetch/PromisesAndFetch.js
@@ -0,0 +1,124 @@
+// Creating Promises
+const promiseOne = new Promise(function(resolve, reject){
+    // Do An async task
+    // 1) DB Calls
+    // 2) Cryptography
+    // 3) Netword
+
+    setTimeout(function(){
+        console.log('Asyn Task Is Complete')
+        resolve() // Connecting With .the()
+    },1000)
+})
+
+// Consuming Promise
+promiseOne.then(function(){
+    console.log("Promise Consumed");
+    
+})
+
+// Doing Both Creating And Consuming In One Place
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        // In real-world applications, real async tasks like fetch() or database queries take an unknown amount of time over the network. When learning or testing Promises, setTimeout is the standard way to create artificial delays so you can see how Promises handle async behavior and timing.
+
+        console.log("Async Task 2")
+        resolve()
+    },1000)
+}).then(function(){
+    console.log("Async 2 Resolved")
+})
+
+// Returning Data From A Promise
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username:"sayamdip", email:"sayamdip@gmail.com"})
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user)
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username:"sayamdip",password:"123"})
+        }
+        else{
+            reject("Something Went Wrong")
+        }
+    },1000)
+})
+
+// Chaining Promise
+const username = promiseFour.then((user)=> {
+    console.log(user)
+    return user.username
+})
+.then((username) => {
+     console.log(username)
+})
+.catch((error)=>{
+    console.log(error)
+})
+.finally(()=>{
+    console.log("Finally, The Promise Is Either Reolved Or Rejected")
+})
+
+console.log(username);
+
+// async and await
+const promiseFive = new Promise((resolve,reject) => {
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username:"javascript",password:"123"})
+        }
+        else{
+            reject("Error! JS Went Wrong")
+        }
+    },1000)
+})
+
+async function consumePromiseFive(){
+    try{
+        const respone = await promiseFive
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+consumePromiseFive()
+
+// Fetch API
+async function getAllUsers(){
+
+    try{
+        const response = await fetch("https://api.github.com/users/sayamdip")
+        const data = await response.json()
+        console.log(data);
+    }
+    catch(error){
+        console.log(`${error} Occured`)
+    }
+    
+}
+getAllUsers()
+
+
+// Fetch With .then .catch
+
+fetch("https://api.github.com/users/sayamdip")
+.then((response) => {
+    return response.json()
+})
+.catch((error) => {
+    console.log(error)
+
+})
+.then((data) => {
+    console.log(data)
+})
